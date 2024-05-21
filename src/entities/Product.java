dc287aae6a516b4c323b720a0b2d38d8aaa16fa7
daslopes <daslopes@outlook.com.br>
2024-05-21 10:58:36 -0300

    Reference method com método não estático

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -25,8 +25,9 @@ public class Product {
     public void setPrice(Double price) {
         this.price = price;
     }
-    public static String staticUpperCaseName(Product p){
-        return p.getName().toUpperCase();
+    
+    public String nonStaticUpperCaseName(){
+        return name.toUpperCase();
     }
 
     @Override
